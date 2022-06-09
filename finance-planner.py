f15c324b87a5ac0fa47a93401af87bc91048b0a7
Artem Sharapko <directed.by.sharapov@gmail.com>
2022-06-09 20:40:35 +0300

    Create a program greeting with the user

---
+++ finance-planner.py
@@ -0,0 +1,3 @@
+# Create a master account
+print("Hi, I am your Financial planner, I will help you with the organization of your finances!")
+# inp = input("Enter the amount and name of the account: ")
