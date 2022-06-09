378f13ed1986bdd7597a31c5828a87af338daf21
Artem Sharapko <directed.by.sharapov@gmail.com>
2022-06-09 21:38:50 +0300

    Create a wish account, create a storage account

--- finance-planner.py
+++ finance-planner.py
@@ -1,6 +1,13 @@
 # Create a master account
 print("Hi, I am your Financial planner, I will help you with the organization of your finances!")
+print("In order for me to save your data, you need to enter the amount and name of the account: 'master' for the main account; the 'wish' to raise money to buy something you really want; 'storage' this is your extra account on which you put money to buy something in the future")
+print("You can only use the following account names: ")
+print("master")
+print("wish")
+print("storage")
 m = "master"
+w = "wish"
+s = "storage"
 a = input("Enter the amount: ")
 n = input("Enter the name of the account: ")
 try:
@@ -11,6 +18,10 @@ except:
 
 if n == m:
     mn = n
+elif n == w:
+    mn = n 
+elif n == s:
+    mn = n    
 else:
     print("You have entered an incorrect account name, will enter a name from the provided options!")
     exit()
