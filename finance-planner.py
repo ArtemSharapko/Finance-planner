1daeadda34530503287de244557967945f54ee54
Artem Sharapko <directed.by.sharapov@gmail.com>
2022-06-09 21:13:33 +0300

    Create a master account

--- finance-planner.py
+++ finance-planner.py
@@ -1,3 +1,18 @@
 # Create a master account
 print("Hi, I am your Financial planner, I will help you with the organization of your finances!")
-# inp = input("Enter the amount and name of the account: ")
+m = "master"
+a = input("Enter the amount: ")
+n = input("Enter the name of the account: ")
+try:
+    ma = float(a)
+except:
+    print("You entered an incorrect amount, enter a numeric value!")
+    exit()
+
+if n == m:
+    mn = n
+else:
+    print("You have entered an incorrect account name, will enter a name from the provided options!")
+    exit()
+
+print(ma, mn)
